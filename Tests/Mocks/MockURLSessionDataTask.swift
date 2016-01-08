4e54cdc8634f54ce7e0d197a5c8dfa696a2ad092
Joe Masilotti <joe@masilotti.com>
2016-01-08 18:21:16 -0500

    Add MockURLSessionDataTask

---
+++ Tests/Mocks/MockURLSessionDataTask.swift
@@ -0,0 +1,15 @@
+//
+//  MockURLSessionDataTask.swift
+//  TestingNSURLSession
+//
+//  Created by Joe Masilotti on 1/8/16.
+//  Copyright © 2016 Masilotti.com. All rights reserved.
+//
+
+class MockURLSessionDataTask: URLSessionDataTaskProtocol {
+    private (set) var resumeWasCalled = false
+
+    func resume() {
+        resumeWasCalled = true
+    }
+}
