52b021d07852d8d9e2cb194beb08c4842a4683d4
Joe Masilotti <joe@masilotti.com>
2016-01-08 07:43:01 -0500

    Initial commit

---
+++ App/HTTPClient.swift
@@ -0,0 +1,13 @@
+//
+//  HTTPClient.swift
+//  TestingNSURLSession
+//
+//  Created by Joe Masilotti on 1/8/16.
+//  Copyright © 2016 Masilotti.com. All rights reserved.
+//
+
+import Foundation
+
+class HTTPClient {
+
+}
---
+++ Tests/HTTPClientTests.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPClientTests.swift
+//  TestingNSURLSessionTests
+//
+//  Created by Joe Masilotti on 1/8/16.
+//  Copyright © 2016 Masilotti.com. All rights reserved.
+//
+
+import XCTest
+
+class HTTPClientTests: XCTestCase {
+    func test_Exists() {
+        XCTAssertNotNil(HTTPClient())
+    }
+}
