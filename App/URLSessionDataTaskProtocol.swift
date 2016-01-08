f6b6a259b8a81fc641fd48bb0784a9952fccc3aa
Joe Masilotti <joe@masilotti.com>
2016-01-08 18:15:14 -0500

    Add URLSessionDataTaskProtocol to test resume()

---
+++ App/URLSessionDataTaskProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  URLSessionDataTaskProtocol.swift
+//  TestingNSURLSession
+//
+//  Created by Joe Masilotti on 1/8/16.
+//  Copyright © 2016 Masilotti.com. All rights reserved.
+//
+
+import Foundation
+
+protocol URLSessionDataTaskProtocol {
+    func resume()
+}
