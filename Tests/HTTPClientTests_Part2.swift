09c63d8dab1b964721ebb4373b2551dcb4ba5ab1
Joe Masilotti <joe@masilotti.com>
2016-01-25 07:02:06 -0500

    Add tests for HTTPClient response (data and error)

--- Tests/HTTPClientTests_Part2.swift
+++ Tests/HTTPClientTests_Part2.swift
@@ -16,4 +16,40 @@ class HTTPClientTests_Part2: XCTestCase {
         super.setUp()
         subject = HTTPClient(session: session)
     }
+
+    func test_GET_WithResponseData_ReturnsTheData() {
+        let expectedData = "{}".dataUsingEncoding(NSUTF8StringEncoding)
+        session.nextData = expectedData
+
+        var actualData: NSData?
+        subject.get(NSURL()) { (data, _) -> Void in
+            actualData = data
+        }
+
+        XCTAssertEqual(actualData, expectedData)
+    }
+
+    func test_GET_WithANetworkError_ReturnsANetworkError() {
+        session.nextError = NSError(domain: "error", code: 0, userInfo: nil)
+
+        var error: ErrorType?
+        subject.get(NSURL()) { (_, theError) -> Void in
+            error = theError
+        }
+
+        XCTAssertNotNil(error)
+    }
+
+    func test_GET_WithResponseDataAndANetworkError_ReturnsAnError() {
+        session.nextData = "{}".dataUsingEncoding(NSUTF8StringEncoding)
+        session.nextError = NSError(domain: "error", code: 0, userInfo: nil)
+
+        var error: ErrorType?
+        subject.get(NSURL()) { (data, theError) -> Void in
+            XCTAssertNil(data)
+            error = theError
+        }
+
+        XCTAssertNotNil(error)
+    }
 }
