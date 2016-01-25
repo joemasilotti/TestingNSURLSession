ffd17ea1b22789b09a94b30ff2762ac0f0e5ee62
Joe Masilotti <joe@masilotti.com>
2016-01-25 07:01:43 -0500

    Add Error, a simple ErrorType

---
+++ App/Error.swift
@@ -0,0 +1,11 @@
+//
+//  Error.swift
+//  TestingNSURLSession
+//
+//  Created by Joe Masilotti on 1/25/16.
+//  Copyright © 2016 Masilotti.com. All rights reserved.
+//
+
+enum Error: ErrorType {
+    case NetworkError
+}
